5c927bcf98e44a2c226fecf9dd5490368508d083
muOb <momo_n5@hotmail.com>
2025-06-04 20:08:01 +0300

    end ch16 and the course

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,7 +3,7 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import postgres from "postgres";
 import { z } from "zod";
-import { signIn } from "@/auth";
+import { signIn } from "@/app/auth";
 import { AuthError } from "next-auth";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
